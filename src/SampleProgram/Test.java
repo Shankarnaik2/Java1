1f2612b6af540a802f8ee6d3f3ed588d9cd60383
Shankarnaik2 <140405202+Shankarnaik2@users.noreply.github.com>
2023-11-25 11:18:06 +0530

    Update Test.java
    New code

--- src/SampleProgram/Test.java
+++ src/SampleProgram/Test.java
@@ -41,10 +41,77 @@ for(int i=0;i<arr1.length;i++) {
 	}
 }
 
-//------------------------------------------------------------------------
-//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
+/*
+		 * String st="madam1"; String temp=""; for(int i=st.length()-1;i>=0;i--) {
+		 * temp+=st.charAt(i); }if(st.equals(temp)) { System.out.println("palindrome");
+		 * }else { System.out.println("not palindrome"); }
+		 */
+		//String st="abcdabcde";
+		/*
+		 * int count=0; for(int i=0;i<st.length();i++) { if(st.charAt(i)=='a') {
+		 * count++; }
+		 * 
+		 * } System.out.println("a word occurance"+count+"times");
+		 */
+		
+		/*
+		 * HashMap<Character, Integer>mp=new HashMap(); for(int i=0;i<st.length();i++) {
+		 * char ch=st.charAt(i); if(mp.containsKey(ch)) { int k=mp.get(ch);k++;
+		 * mp.put(ch, k); }else { mp.put(ch,1); } }
+		 * 
+		 * 
+		 * for(Character key:mp.keySet()) { System.out.print(key+""+mp.get(key));
+		 * 
+		 * }
+		 */
+		/*
+		 * int arr[]= {2,5,4,33,9,55}; int temp=0; for(int i=0;i<arr.length;i++) {
+		 * for(int j=i+1;j<arr.length;j++) { if(arr[i]>arr[j]) { temp=arr[i];
+		 * arr[i]=arr[j]; arr[j]=temp; } } } for(int i=0;i<arr.length;i++) {
+		 * System.out.println(arr[i]); }
+		 */
+		/*
+		 * int count=10; int a=0,b=1; for(int i=1;i<count;i++) {
+		 * System.out.print(a+" "); int c=a+b; a=b; b=c; }
+		 */
+		/*
+		 * String st="java is a programming language"; int count=1; String
+		 * arr[]=st.split(" "); for(int i=0;i<st.length();i++) { if(st.charAt(i)==' ' &&
+		 * st.charAt(i+1)!=' ') { count++; } } System.out.println(count);
+		 */
+		/*
+		 * int a=23; boolean flag=false; for(int i=2;i<=a/2;i++) { if(a%i==0) {
+		 * flag=true; break; } } if(!flag) { System.out.println("prime number"); }else {
+		 * System.out.println("not prime number"); }
+		 */
+		/*
+		 * int arr[]= {1,4,6,455,44,3,99,999}; int temp=0; int len=arr.length; for(int
+		 * i=0;i<arr.length;i++) { for(int j=i+1;j<arr.length;j++) { if(arr[i]<arr[j]) {
+		 * temp=arr[i]; arr[i]=arr[j]; arr[j]=temp; } } } //for(int
+		 * i=0;i<arr.length;i++) { //smallest System.out.println(arr[len-1]); //second
+		 * smallest System.out.println(arr[len-2]); //biggest
+		 * 
+		 * System.out.println(arr[0]); System.out.println(arr[1]);
+		 */
+		/*
+		 * String st="java is programming"; String arr[]=st.split(" "); String temp="";
+		 * for(int i=arr.length-1;i>=0;i--) { temp=temp+arr[i]+" "; }
+		 * System.out.println(temp);
+		 */
+		
+		String st="java is a programming launguage";
+		String arr[]=st.split(" ");
+		String temp="";
+		String out="";
+		for(int i=0;i<arr.length;i++) {
+			String word=arr[i];
+			
+		for(int j=word.length()-1;j>=0;j--) {
+			temp+=word.charAt(j);
+		} 
+		
+		out+=temp+" ";
+		}
+		System.out.print(out);
+	}}
 
-
-	}
-
-}
